04c254ac0b17bee81afb493c0265d888a881a98f
Constant Dupuis <constant.dupuis@gmail.com>
2022-10-28 15:10:22 +0200

    Add New input system and make the flipper going back to start position when trigger is released.

--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -1,29 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(HingeJoint))]
 public class Flipper : MonoBehaviour
 {
-    public float flipStrength;
+    public float flipStrength = 10;
 
-    Rigidbody rigidBody;
-    Vector3 torque;
+    //private Rigidbody rigidBody;
+    private HingeJoint hinge;
+
+    private JointMotor motor;
+
+    //private Vector3 torque;
 
     private void Awake()
     {
-        rigidBody = GetComponent<Rigidbody>();
-        rigidBody.centerOfMass = Vector3.zero;
-        Push(false);
+        //rigidBody = GetComponent<Rigidbody>();
+        //rigidBody.centerOfMass = Vector3.zero;
+
+        hinge = GetComponent<HingeJoint>(); 
+        hinge.useMotor = true;
+        //motor = new JointMotor();
+        motor = hinge.motor;
+        motor.force = flipStrength;
+        //motor.freeSpin = false;
+        //motor.targetVelocity = 180;
+
+        //Push(true);
     }
 
-    public void Push(bool isPushed)
-    { 
-        torque = transform.forward * flipStrength * (isPushed ? -1 : 1);
-    }
-
-    private void FixedUpdate()
+    public void Push(InputAction.CallbackContext context)
     {
-        rigidBody.AddTorque(torque);
+        Debug.Log($"Started : {context.started}, Performed : {context.performed}, Canceled : {context.canceled},");
+        //torque = transform.forward * flipStrength * (isPushed ? -1 : 1);
+        //if (context.performed)
+        //{
+        //    motor.targetVelocity = 360;
+        //}
+        //else {
+        //    motor.targetVelocity = -360;
+        //}
+        motor.targetVelocity = (context.performed ? 1 : -1) * 360;
+        //motor.force = flipStrength * (context.performed ? -1 : 1);
+        hinge.motor = motor;
     }
 }
